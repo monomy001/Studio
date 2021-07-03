bd84a5bf7b1dff8642ed301d5a344e0ae8bb5d0e
Modular Economy <info@tansah.com>
2021-07-03 10:05:35 +0700

    initial push after fork

---
+++ studio.js
@@ -0,0 +1,14 @@
+monogatari.component ('studio').template (() => {
+	return `
+		<h1>Monogatari studio</h1>
+		<div data-content="visuals">
+			tabs:<ol>
+                <li>Game Options</li>
+                <li>Characters</li>
+                <li>Assets [scene,image,music,voice etc]</li>
+                <li>plot</li>
+                <li>keyboard shortcuts</li>
+            </ol>
+		</div>
+	`;
+});
